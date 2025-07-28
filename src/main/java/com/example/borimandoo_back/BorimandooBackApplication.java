c04f60878e24e3a2686859a5ab455959b09b6f25
gyesswhat <h2weirdo@icloud.com>
2025-07-28 18:00:07 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/example/borimandoo_back/BorimandooBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.borimandoo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorimandooBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorimandooBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/borimandoo_back/BorimandooBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.borimandoo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorimandooBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
